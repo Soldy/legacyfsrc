32cb8c8b0b6f1e018e55c47cef22e600fcf2c7b6
Soldy <4786022+Soldy@users.noreply.github.com>
2023-04-23 13:41:56 +0100

    fast code
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ legacyfiles.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const currentfilesBase = function(){
+    /**
+     * 
+     * @param {string}
+     * @param {string}
+     * @public
+     * @return {string}
+     */
+    this.readFile = async function(file, encoding = 'utf8'){
+        return await _fs.readFile(file, {encoding});
+    };
+    const _fs = require('node:fs/promises');
+};
+
+const legacyFilesBase = function(){
+    /**
+     * 
+     * @param {string}
+     * @param {string}
+     * @public
+     * @return {string}
+     */
+    const readFile = function (file, encoding = 'utf8'){
+        return new Promise((resolve) => {
+            _fs.readFile(file, encoding, function (error, data) {
+                if (error) throw Error(error);
+                resolve(data);
+            });
+        });
+    };
+    const _fs = require('fs');
+};
+
+
+if(parseInt((process.versions.node).split('.')[0]) > 9){
+    exports.base = currentfilesBase;
+}else{
+    exports.base = legacyFilesBase;
+}
