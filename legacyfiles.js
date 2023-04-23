d9f89ee521e09173afb342d1cd1e6378cbf24f4c
Soldy <4786022+Soldy@users.noreply.github.com>
2023-04-23 13:49:01 +0100

    version check improve
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- legacyfiles.js
+++ legacyfiles.js
@@ -22,7 +22,7 @@ const legacyFilesBase = function(){
      * @public
      * @return {string}
      */
-    const readFile = function (file, encoding = 'utf8'){
+    this.readFile = function (file, encoding = 'utf8'){
         return new Promise((resolve) => {
             _fs.readFile(file, encoding, function (error, data) {
                 if (error) throw Error(error);
@@ -34,7 +34,10 @@ const legacyFilesBase = function(){
 };
 
 
-if(parseInt((process.versions.node).split('.')[0]) > 9){
+if(
+   (parseInt((process.versions.node).split('.')[0]) > 15) ||
+   (parseInt((process.versions.node).split('.')[0]) === 14)
+){
     exports.base = currentfilesBase;
 }else{
     exports.base = legacyFilesBase;
